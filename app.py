7c00eefc15cd49f00567d13da799cb3f2ed0487e
JeanGReis <jean.g.reis@hotmail.com>
2022-06-12 23:54:20 +0000

    Alteraçao

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 # Ao abrir o GitPod, execute:
 # pip install -r requirements.txt
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from uuid import uuid4
+import csv
 
 app = Flask(__name__)
 
 prisioneiros = [
     {'id': 1, 'nome': 'Túlio', 'crime': 'Assasinato de duas jovens', 'tempo': '10 anos'},
     {'id': 2, 'nome': 'Roberto', 'crime': 'Furto de carro', 'tempo': '4 anos'},
-    {'id': 3, 'nome': 'Jorge', 'crime': 'Amar demais', 'tempo': 'Prisão perpétua'},
+    {'id': 3, 'nome': 'Ibere', 'crime': 'Amar demais', 'tempo': 'Prisão perpétua'},
 ]
 
 @app.route('/')
@@ -35,41 +36,29 @@ def save():
 
 @app.route('/delete/<id>')
 def delete(id):
-    return id
+    for prisioneiro in prisioneiros:
+        if ( id == str(prisioneiro['id'])):
+            i = prisioneiros.index(prisioneiro)
+            del prisioneiros[i]
+            return redirect('/')
 
-@app.route('/update/<id>')
-def update(id):
-    return id
+@app.route('/edit/<id>')
+def edit(id):
+    for prisioneiro in prisioneiros:
+        if id == str(prisioneiro['id']):
+            return render_template('update.html',prisioneiro = prisioneiro)
+
+@app.route('/edit/prisioneiro/<id>', methods=['POST'])
+def salvar_edicao(id):
+    for prisioneiro in prisioneiros:
+        if (id == str(prisioneiro['id'])):
+            i = prisioneiros.index(prisioneiro)
+            id_modificado = prisioneiro['id']
+    upNome = request.form['nome']
+    upCrime = request.form['crime']
+    upTempo = request.form['tempo']
+    prisioneiros[i] = {'id':id_modificado,'nome':upNome,'crime':upCrime,'tempo':upTempo}
+    return redirect('/')
 
 app.run(debug=True)
-
-
-
-
-
-# CLIENTE -- SERVIDOR
-# Navegador -- AWS (Flask)
-
-# Client -> REQUEST (Mensagem HTTP) -> Server 
-# Server -> RESPONSE (Mensagem HTTP) -> CLIENTE
-
-# HTTP (HyperText Transfer Protocol)
-# HTML (HyperText Markup Language)
-
-# Mensagem HTTP: 
-# Header
-# Body
-# METHOD (GET, POST), Métodos suportados pelos navegadores.
-# GET -> DADOS PELA URL
-# POST -> OCULTO OS DADOS (NÃO MOSTRA NA URL)
-
-# METHOD (API = GET, POST, PUT, DELETE, PATCH, OPTIONS)
-
-# API REST
-# POST   (C)REATE
-# GET    (R)EAD
-# PUT    (U)PDATE
-# PATCH  (U)PDATE PARCIAL
-# DELETE (D)ELETE
-
 
